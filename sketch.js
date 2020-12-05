935e22cadec771e225f5dc0aebb7f5aa6ec39381
rujutha <bhavnabhansali01@gmail.com>
2020-12-05 16:43:09 +0530

    game

--- sketch.js
+++ sketch.js
@@ -1,29 +1,36 @@
-var ball;
+var canvas,backgroundImage;
+
+var gameState=0;
+var playerCount;
+var allPlayers;
+var distance=0;
+
+
+
+var database;
+var form,player,game;
+var car1,car2,car3,car4,cars;
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+canvas=createCanvas(displayWidth-20,displayHeight-30);
+database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+
+
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+if(playerCount===4){
+    game.update(1);
+
+}
+if(gameState===1){
+    clear ();
+    game.play();
+    
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+
 }
